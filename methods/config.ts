8a5e47f43408f55bb6b5ccb8af0d0f5356ec42eb
p1ay2invokio <play21947@gmail.com>
2025-06-14 08:40:12 +0700

    uri-fix

--- methods/config.ts
+++ methods/config.ts
@@ -1,6 +1,6 @@
 export const uri = `https://attend.playapi.shop`
-// export const uri_maekhan = `https://192.168.1.7`
+export const uri_maekhan = `https://192.168.1.7`
 // export const uri_maekhan = `http://localhost:3001`
-export const uri_maekhan = `https://192.168.1.7:3001`
+// export const uri_maekhan = `https://192.168.1.7:3001`
 // `https://attend.playapi.shop`
 // `http://localhost:3001`
