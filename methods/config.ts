f57f8b602baa47a5390b7759e038a55101611db1
p1ay2invokio <play21947@gmail.com>
2025-07-19 00:19:10 +0700

    fix/url to 88

--- methods/config.ts
+++ methods/config.ts
@@ -1,6 +1,6 @@
 export const uri = `https://attendduck.playapi.shop`
-export const uri_maekhan = `https://192.168.1.7`
-export const uri_sanpatong = `https://192.168.1.29`
+export const uri_maekhan = `https://192.168.1.88`
+export const uri_sanpatong = `https://192.168.1.88`
 // export const uri_maekhan = `http://localhost:3001`
 // export const uri_maekhan = `http://192.168.1.7:3001`
 // `https://attend.playapi.shop`
