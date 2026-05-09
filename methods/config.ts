cc1ba24844c774a775d7024983ea26ce8d02cd91
fiw <66400989+p1ay2invokio@users.noreply.github.com>
2026-05-09 16:40:38 +0700

    fix: config url

--- methods/config.ts
+++ methods/config.ts
@@ -1,7 +1,7 @@
 export const uri = `https://attend.whanjeabs.online`
-export const uri_maekhan = `https://apis.maekhan.shop:8443`
-export const uri_sanpatong = `https://sanpatongapis.maekhan.shop:8443`
-export const uri_doilor = `https://doilorapis.maekhan.shop:8443`
+export const uri_maekhan = `http://maekhan2020.duckdns.org:3001`
+export const uri_sanpatong = `http://sanpatongddns.duckdns.org:3001`
+export const uri_doilor = `http://doilorddns.duckdns.org:3001`
 
 // export const uri_maekhan = `http://localhost:3001`
 // export const uri_maekhan = `http://192.168.1.7:3001`
